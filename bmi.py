dec36d6bbee32a55dc08c2ffd4c83a12d32940fc
Veron <veron05.23@ichat.sp.edu.sg>
2024-10-28 15:18:35 +0800

    Add return value

--- bmi.py
+++ bmi.py
@@ -1,16 +1,22 @@
 def calculate_bmi(height, weight):
-    print("Height="+str(height))
-    print("Weight="+str(weight))
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
 
-    bmi=weight/(height*height)
-    print("BMI="+str(bmi))
+    bmi = weight / (height * height)
+    print("BMI=" + str(bmi))
 
-    if bmi<18.5:
+    if bmi < 18.5:
         print("Weight Classification=Under Weight")
-    elif bmi>=18.5 and bmi<=25.0:
+        return_value = -1
+    elif 18.5 <= bmi <= 25.0:
         print("Weight Classification=Normal Weight")
+        return_value = 0
     else:
         print("Weight Classification=Over Weight")
+        return_value = 1
     
+    return return_value
 
-calculate_bmi(weight=57,height=1.73)
+# Call the function and store the result
+result = calculate_bmi(weight=57, height=1.73)
+print("Return value=" + str(result))

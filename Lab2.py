4833551f9f1ace43586ad72a97d92376554c20f1
Veron <veron05.23@ichat.sp.edu.sg>
2024-11-18 02:08:13 +0800

    version 3.0

--- Lab2.py
+++ Lab2.py
@@ -7,27 +7,28 @@ def get_user_input():
     num_list = [float(num) for num in num_list]
     return num_list
 
-def calc_average_temperature(num_list):
+def calc_average(num_list):
     total_sum=sum(num_list)
     avg_temp=total_sum/len(num_list)
-    print("Average temperature="+str(avg_temp))
+    return avg_temp
 
-def calc_min_max_temperature(num_list):
-    print("Min_Max temperature=["+str(min(num_list))+", "+str(max(num_list))+"]")
+def find_min_max(num_list):
+    return [min(num_list), max(num_list)]
 
 def calc_median_temperature(num_list):
     num_list.sort()
     mid = len(num_list)//2
     res = (num_list[mid]+num_list[~mid])/2
-    print("Median temperature="+str(res))
+    return res
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
     print(num_list)
-    calc_average_temperature(num_list)
-    calc_min_max_temperature(num_list)
-    calc_median_temperature(num_list)
+    print("Average temperature="+str(calc_average(num_list)))
+    print("Min_Max temperature="+str(find_min_max(num_list)))
+    print("Median temperature="+str(calc_average(num_list)))
 
-main()
+if __name__ == "__main__":
+    main()
